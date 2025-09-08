72142a17a35085ed666ec2b528d96b6447d8f3c3
eduardofreirelage-source <eduardofreirelage@gmail.com>
2025-09-08 17:37:17 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,11 +1,6 @@
-// supabase-client.js
-
-// Importa a biblioteca do Supabase
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Suas credenciais do Supabase
 const supabaseUrl = 'https://wadbknlbrzixuadpuuoz.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhZGJrbmxicnppeHVhZHB1dW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDk3OTgsImV4cCI6MjA3MjkyNTc5OH0.KUu1jUgbhJxUuiZjJmeSViLbf73oNVqhVr7QMj6TPos';
 
-// Cria e exporta o cliente Supabase para ser usado em outros arquivos
 export const supabase = createClient(supabaseUrl, supabaseKey);
